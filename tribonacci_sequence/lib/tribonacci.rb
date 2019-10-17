0cd000b610025714d6dc9e46fdcf08546db2b05f
Vanessa <vane.gomez@me.com>
2019-10-17 11:27:42 -0600

    solution 2

--- tribonacci_sequence/lib/tribonacci.rb
+++ tribonacci_sequence/lib/tribonacci.rb
@@ -5,18 +5,13 @@ class Tribonacci
 	end
 
 	def sequence
-		return [] if @length == 0
-		return [@signature[0]] if @length == 1
-		return @signature.first(2) if @length == 2
-		return @signature if @length == 3
-
-		(@length - 3).times do
-			@signature << @signature[-3..-1].reduce(:+)
+		if @length < 4
+			@signature.first(@length)
+		else
+			(@length - 3).times do
+				@signature << @signature.last(3).reduce(:+)
+			end
+			@signature
 		end
-		@signature
 	end
 end
-
-# Solution 2
-#  n < 4 ? signature = signature.first(n) : (n-3).times { signature << signature.last(3).inject(:+) }
-#  signature
